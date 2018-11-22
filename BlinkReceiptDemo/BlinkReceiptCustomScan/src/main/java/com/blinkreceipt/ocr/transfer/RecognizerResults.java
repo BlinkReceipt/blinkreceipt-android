99bc3c8ffd0f4274dc61addc27cd978e9939c81f
Christopher Brown <christopher.brown@priceline.com>
2018-11-22 12:13:13 -0500

    1.1.4

---
+++ BlinkReceiptDemo/BlinkReceiptCustomScan/src/main/java/com/blinkreceipt/ocr/transfer/RecognizerResults.java
@@ -0,0 +1,68 @@
+package com.blinkreceipt.ocr.transfer;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.microblink.Media;
+import com.microblink.ScanResults;
+
+public final class RecognizerResults {
+
+    @Nullable
+    private ScanResults results;
+
+    @Nullable
+    private Media media;
+
+    @Nullable
+    private Throwable e;
+
+    public RecognizerResults( @Nullable ScanResults results, @Nullable Media media ) {
+        this( results, media, null );
+    }
+
+    public RecognizerResults( @NonNull Throwable e ) {
+        this( null, null, e );
+    }
+
+    private RecognizerResults(@Nullable ScanResults results, @Nullable Media media, @Nullable Throwable e) {
+        super();
+
+        this.results = results;
+
+        this.media = media;
+
+        this.e = e;
+    }
+
+    @Nullable
+    public ScanResults results() {
+        return results;
+    }
+
+    @Nullable
+    public Media media() {
+        return media;
+    }
+
+    @Nullable
+    public Throwable e() {
+        return e;
+    }
+
+    public RecognizerResults e(@NonNull Throwable e ) {
+        this.e = e;
+
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "RecognizerResults{" +
+                "results=" + results +
+                ", media=" + media +
+                ", e=" + e +
+                '}';
+    }
+
+}
