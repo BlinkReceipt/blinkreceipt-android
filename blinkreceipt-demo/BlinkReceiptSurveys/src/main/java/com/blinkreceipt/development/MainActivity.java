2590d9578158c436711b516b1e5e069dc2f018b7
nadirw9 <nadir.williams@onepeloton.com>
2020-12-06 21:42:48 -0500

    Blink Surveys Demo

--- blinkreceipt-demo/BlinkReceiptSurveys/src/main/java/com/blinkreceipt/development/MainActivity.java
+++ blinkreceipt-demo/BlinkReceiptSurveys/src/main/java/com/blinkreceipt/development/MainActivity.java
@@ -1,6 +1,5 @@
 package com.blinkreceipt.development;
 
-import android.view.View;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,20 +17,15 @@ public class MainActivity extends AppCompatActivity {
 
         SurveyViewModel viewModel = new ViewModelProvider(this).get(SurveyViewModel.class);
 
-        findViewById(R.id.load_survey_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new SurveyLoader().loadSurveys()
-                        .addOnSuccessListener(surveys -> {
-                            if (surveys != null && !surveys.isEmpty()) {
-                                viewModel.survey(surveys.get(0));
-                            } else {
-                                Toast.makeText(MainActivity.this, "RESPONSE NULL OR EMPTY", Toast.LENGTH_SHORT).show();
-                            }
-                        }).addOnFailureListener(
-                        e -> Toast.makeText(MainActivity.this, "RESPONSE ERROR: " + e.getMessage(), Toast.LENGTH_SHORT).show());
-            }
-        });
+        findViewById(R.id.load_survey_btn).setOnClickListener(v -> new SurveyLoader().loadSurveys()
+                .addOnSuccessListener(surveys -> {
+                    if (surveys != null && !surveys.isEmpty()) {
+                        viewModel.survey(surveys.get(0));
+                    } else {
+                        Toast.makeText(MainActivity.this, "RESPONSE NULL OR EMPTY", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(
+                e -> Toast.makeText(MainActivity.this, "RESPONSE ERROR: " + e.getMessage(), Toast.LENGTH_SHORT).show()));
 
         SurveyFragment surveyFragment = SurveyFragment.newInstance(R.style.DemoSurvey);
 
