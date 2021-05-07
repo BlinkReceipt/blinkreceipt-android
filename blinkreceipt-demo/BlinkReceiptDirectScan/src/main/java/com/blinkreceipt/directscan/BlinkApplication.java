f0446d156341e551465f1625e17c3c75826f3459
nadir <nadir.n.williams@gmail.com>
2021-05-07 00:44:02 -0400

    Direct api comments

--- blinkreceipt-demo/BlinkReceiptDirectScan/src/main/java/com/blinkreceipt/directscan/BlinkApplication.java
+++ blinkreceipt-demo/BlinkReceiptDirectScan/src/main/java/com/blinkreceipt/directscan/BlinkApplication.java
@@ -1,4 +1,15 @@
 package com.blinkreceipt.directscan;
 
-public class BlinkApplication {
+import android.app.Application;
+import com.microblink.BlinkReceiptSdk;
+
+public class BlinkApplication extends Application {
+
+    @Override
+    public void onTerminate() {
+        BlinkReceiptSdk.terminate();
+
+        super.onTerminate();
+    }
+
 }
