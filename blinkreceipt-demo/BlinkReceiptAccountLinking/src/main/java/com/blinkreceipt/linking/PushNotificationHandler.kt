425969b9fe04ee0831a7d9afda14d2b0b2303968
Patrick Bu <patrick.bu@microblink.com>
2025-04-28 18:10:21 -0700

    [CDR-2477] BlinkReceipt Background Refresh

---
+++ blinkreceipt-demo/BlinkReceiptAccountLinking/src/main/java/com/blinkreceipt/linking/PushNotificationHandler.kt
@@ -0,0 +1,18 @@
+package com.blinkreceipt.linking
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class PushNotificationHandler: FirebaseMessagingService() {
+
+    private val messagingClient
+        get() = (application as BlinkApplication).messagingClient
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        messagingClient.onMessageReceived(message)
+    }
+
+    override fun onNewToken(token: String) {
+        messagingClient.onNewToken(token)
+    }
+}
