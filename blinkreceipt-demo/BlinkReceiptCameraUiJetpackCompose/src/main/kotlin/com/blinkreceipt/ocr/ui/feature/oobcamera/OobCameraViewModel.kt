e5b7ef133c132a6121358916c03b31399e57ce14
Lawrence C. Cendaña <162448417+microblink-lcc@users.noreply.github.com>
2025-11-18 01:42:12 +0800

    Update OobCameraViewModel.kt
    - Fix Bug: Camera Cancel Leaves UI Trapped
    - Bug: Silent Scan Failures Confuse Users

--- blinkreceipt-demo/BlinkReceiptCameraUiJetpackCompose/src/main/kotlin/com/blinkreceipt/ocr/ui/feature/oobcamera/OobCameraViewModel.kt
+++ blinkreceipt-demo/BlinkReceiptCameraUiJetpackCompose/src/main/kotlin/com/blinkreceipt/ocr/ui/feature/oobcamera/OobCameraViewModel.kt
@@ -1,6 +1,5 @@
 package com.blinkreceipt.ocr.ui.feature.oobcamera
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.blinkreceipt.ocr.models.toProductItems
@@ -16,7 +15,6 @@ import javax.inject.Inject
 @HiltViewModel
 class OobCameraViewModel @Inject constructor(
     private val productItemsFileService: ProductItemsFileService,
-    private val savedStateHandle: SavedStateHandle,
 ): ViewModel() {
 
     private val _events: MutableSharedFlow<OobCameraEvent> = MutableSharedFlow()
@@ -28,7 +26,6 @@ class OobCameraViewModel @Inject constructor(
                 is OobCameraAction.ReceiveRecognizerResults -> {
                     when (val results = action.results) {
                         is CameraRecognizerResults.Finishing -> {
-                            savedStateHandle[KEY_IS_PROCESSING_SCAN_RESULTS] = true
                             _events.emit(OobCameraEvent.OnFinishing)
                         }
                         is CameraRecognizerResults.Success -> {
@@ -51,23 +48,17 @@ class OobCameraViewModel @Inject constructor(
                                     OobCameraEvent.OnError
                                 )
                             }
-
-                            savedStateHandle[KEY_IS_PROCESSING_SCAN_RESULTS] = false
                         }
                         is CameraRecognizerResults.Exception -> {
-                            savedStateHandle[KEY_IS_PROCESSING_SCAN_RESULTS] = false
+                            _events.emit(OobCameraEvent.OnError)
                         }
                         is CameraRecognizerResults.Cancelled -> {
-                            savedStateHandle[KEY_IS_PROCESSING_SCAN_RESULTS] = false
+                            _events.emit(OobCameraEvent.OnCancelled)
                         }
                     }
                 }
             }
         }
-    }
-
-    companion object {
-        private const val KEY_IS_PROCESSING_SCAN_RESULTS = "isProcessingScanResults"
     }
 
 }
