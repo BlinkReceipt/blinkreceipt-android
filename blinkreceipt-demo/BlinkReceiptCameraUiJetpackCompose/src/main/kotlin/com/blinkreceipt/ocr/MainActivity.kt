d33ef367f7f8e9fb25b14c2795df3b1fd02f1a5d
Lawrence C. Cendaña <162448417+microblink-lcc@users.noreply.github.com>
2025-11-18 19:30:41 +0800

    Update MainActivity.kt
    - Remove unused import

--- blinkreceipt-demo/BlinkReceiptCameraUiJetpackCompose/src/main/kotlin/com/blinkreceipt/ocr/MainActivity.kt
+++ blinkreceipt-demo/BlinkReceiptCameraUiJetpackCompose/src/main/kotlin/com/blinkreceipt/ocr/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.blinkreceipt.ocr
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.fragment.app.FragmentActivity
